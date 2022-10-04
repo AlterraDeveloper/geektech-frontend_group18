a763fec01da9ee073e4035532d48e5ddb0d02baa
tilekF <gchncbv@gmail.com>
2022-10-04 19:35:04 +0600

    DZ

--- Lesson_8/Constants.js
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_SEPARATOR = "%";
-export const BACKEND_URL = "test.backend:7000";
--- Lesson_8/Task.js
+++
@@ -1,7 +0,0 @@
-export default class Task {
-  constructor(id, text, isCompleted = false) {
-    this.id = id;
-    this.text = text;
-    this.isCompleted = isCompleted;
-  }
-}
